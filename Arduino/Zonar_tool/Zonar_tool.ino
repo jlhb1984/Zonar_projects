f83224f37a5fa95154c5d876c1aad3f370d50c97
Jose Luis Hurtado Balcazar <jose.hurtado@zonarsystems.com>
2026-04-09 12:45:46 -0500

    Comments on Montvoltin and Zonar_tool.

--- Arduino/Zonar_tool/Zonar_tool.ino
+++ Arduino/Zonar_tool/Zonar_tool.ino
@@ -5,6 +5,9 @@
 /*
 The program starts with clock signal defined by OCR1A registrer placed on void setup(). To turn off the clock, follow 
 this options: x+6+cad.
+PD6: ANALOG INPUT 6 IN+ comparator.
+PD7: ANALOG INPUT 7 IN- comparator.
+PB1: DIGITAL OUTPUT 9 Clock signal.
 */
 int val,apin0=A0,apin1=A1,apin2=A2,apin3=A3,apin4=A4,apin5=A5,dpin2=2,dpin13=13,dpin9=9,compin0=6,compin1=7;
 char op,clock_op;
@@ -33,7 +36,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(dpin13,OUTPUT);
   pinMode(dpin2,OUTPUT);
-  pinMode(dpin9,OUTPUT);  //0C1A
+  pinMode(dpin9,OUTPUT);  //0C1A.
   pinMode(compin0,INPUT);
   pinMode(compin1,INPUT);
   //Clock:  
@@ -63,7 +66,7 @@ void loop() {
 
 void serialEvent(){
   op=Serial.read();
-  Serial.print("0 CAD0, 1 CAD1, 2 CAD2, 3 CAD3, 4 CAD4, 5 CAD5 6 CLOCK OFF 7 One Wire.");
+  Serial.print("\n\n 0 CAD0.\n 1 CAD1.\n 2 CAD2.\n 3 CAD3.\n 4 CAD4.\n 5 CAD5.\n 6 CLOCK OFF.\n 7 One Wire.");
   
   switch(op){
     case '0':
@@ -71,7 +74,7 @@ void serialEvent(){
       val=analogRead(apin0);
       Serial.println(val); 
       op=Serial.read();  
-      }while(op!='x');// del do
+      }while(op!='x');// del do.
       break;
 
     case '1':
@@ -79,7 +82,7 @@ void serialEvent(){
       val=analogRead(apin1);
       Serial.println(val); 
       op=Serial.read();  
-      }while(op!='x');// del do
+      }while(op!='x');// del do.
       break;
 
     case '2':
@@ -87,7 +90,7 @@ void serialEvent(){
       val=analogRead(apin2);
       Serial.println(val); 
       op=Serial.read();  
-      }while(op!='x');// del do
+      }while(op!='x');// del do.
       break;
 
     case '3':
@@ -95,7 +98,7 @@ void serialEvent(){
       val=analogRead(apin3);
       Serial.println(val); 
       op=Serial.read();  
-      }while(op!='x');// del do
+      }while(op!='x');// del do.
       break;
 
     case '4':
@@ -103,7 +106,7 @@ void serialEvent(){
       val=analogRead(apin4);
       Serial.println(val); 
       op=Serial.read();  
-      }while(op!='x');// del do
+      }while(op!='x');// del do.
       break;
 
     case '5':
@@ -111,7 +114,7 @@ void serialEvent(){
       val=analogRead(apin5);
       Serial.println(val); 
       op=Serial.read();  
-      }while(op!='x');// del do
+      }while(op!='x');// del do.
       break;
 
     case '6':
@@ -169,19 +172,19 @@ void serialEvent(){
 
     ds.reset();
     ds.select(addr);
-    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+    ds.write(0x44, 1);        // start conversion, with parasite power on at the end.
     
-    delay(1000);     // maybe 750ms is enough, maybe not
+    delay(1000);     // maybe 750ms is enough, maybe not.
     // we might do a ds.depower() here, but the reset will take care of it.
     
     present = ds.reset();
     ds.select(addr);    
-    ds.write(0xBE);         // Read Scratchpad
+    ds.write(0xBE);         // Read Scratchpad.
 
     Serial.print("  Data = ");
     Serial.print(present, HEX);
     Serial.print(" ");
-    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes.
       data[i] = ds.read();
       Serial.print(data[i], HEX);
       Serial.print(" ");
@@ -196,18 +199,18 @@ void serialEvent(){
     // even when compiled on a 32 bit processor.
     int16_t raw = (data[1] << 8) | data[0];
     if (type_s) {
-      raw = raw << 3; // 9 bit resolution default
+      raw = raw << 3; // 9 bit resolution default.
       if (data[7] == 0x10) {
-        // "count remain" gives full 12 bit resolution
+        // "count remain" gives full 12 bit resolution.
         raw = (raw & 0xFFF0) + 12 - data[6];
       }
     } else {
       byte cfg = (data[4] & 0x60);
-      // at lower res, the low bits are undefined, so let's zero them
-      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-      //// default is 12 bit resolution, 750 ms conversion time
+      // at lower res, the low bits are undefined, so let's zero them.
+      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms.
+      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms.
+      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms.
+      //// default is 12 bit resolution, 750 ms conversion time.
     }
     celsius = (float)raw / 16.0;
     fahrenheit = celsius * 1.8 + 32.0;
@@ -222,4 +225,4 @@ void serialEvent(){
     Serial.print(" Fin");    
     }// del switch.  
  
-}// del serialEvent
+}// del serialEvent.
---
+++ MPLAB/MONTVOLTINC.X/MONTVOLTIN.c
@@ -0,0 +1,70 @@
+/*
+ * File:   MONTVOLTIN.c
+ * Author: jlhb1
+ *
+ * Created on 7 de marzo de 2026, 04:21 PM.
+ * Analog input: ADC0 PA0.
+ * INT0: PD2.
+ * OC1A: PD5.
+ * TX: PD1.
+ * PB0: TIMER1 COMP ON.
+ */
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h> // Essential for ISR and sei().
+#include <util/delay.h>
+
+uint8_t low_byte;
+uint8_t high_byte;
+
+// 1. Define the ISR (Interrupt Service Routine).
+// This code runs AUTOMATICALLY when PD2 hits the ground.
+ISR(INT0_vect) {
+    cli();
+    ADCSRA|=(1 << ADSC);
+    //PORTB=0b00000000;
+    sei();
+}
+
+ISR(ADC_vect){
+    cli();
+    low_byte=ADCL;
+    high_byte=ADCH,
+    //PORTB=ADCL;
+    OCR1AL=low_byte;
+    OCR1AH=high_byte;
+    TIMSK|=(1<<OCIE1A);
+    sei();
+}
+
+ISR(TIMER1_COMPA_vect){
+    cli();    
+    PORTB|=(1<<PB0);
+    UDR=0b10111111;
+    sei();
+}
+
+int main(void) {
+    DDRA=0b11111110;
+    DDRB=0b11110011;    
+    DDRD=0b01110010;    //OC2_IN,ICP1_OUT,OC1A_OUT,OC1B_OUT,INT1_IN,INT0_IN,TXD_OUT,RXD_IN.
+    MCUCR=0b00000011;   //RISING EDGE.
+    GICR=0b01000000;    //INT0 ENABLE.
+    ADCSRA=0b10001000;  //ADEN_1,ADCS_START_0,ADATE_0,ADIF,ADIE_1,ADPS2_0,ADPS1_0,ADPSO_0.
+    TCCR1A=0b01000000;  //COM1A1_1,COM1A0_1,COM1B1_0,COM1B0_0,FOC1A_0,FOCIB_0,WGM11_0,WGN10_0.
+    TCCR1B=0b00001001;  //ICNC1_0,ICES1_0,-,WGM13_0,WGM12_1,CS12_0,CS11_0,CS10_1.
+    
+    UBRRH=0b00000000;
+    UBRRL=0b00011001;   //2400 BAUDIOS.
+    UCSRA=0b00000000;
+    UCSRC=0b10000110;
+    UCSRB=0b10001000;
+    sei();    
+
+    while (1) {
+        //sei();
+        // Your main code lives here. It can be doing anything.
+        // (or nothing), and the interrupt will still work!
+    }
+}
