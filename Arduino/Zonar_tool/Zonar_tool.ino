bb01a8e704530dc043707e54b9e2f3c6e6e5b821
Jose Luis Hurtado Balcazar <jose.hurtado@zonarsystems.com>
2026-05-22 09:52:44 -0500

    First Zonar_tool for Arduino Nano 33IoT.

--- Arduino/Zonar_tool/Zonar_tool.ino
+++ Arduino/Zonar_tool/Zonar_tool.ino
@@ -1,423 +1,83 @@
-#include <SoftwareSerial.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <OneWire.h>
-
 /*
-The program starts with clock signal defined by OCR1A registrer placed on void setup(). To turn off the clock, follow 
-this options: x+6+cad.
-PD6: ANALOG INPUT 6 IN+ comparator.
-PD7: ANALOG INPUT 7 IN- comparator.
-PB1: DIGITAL OUTPUT 9 Clock signal.
-PB2: DIGITAL INPUT 10 RX.
-PB3: DIGITAL OUTPUT 11 TX.
-*/
-int val,apin0=A0,apin1=A1,apin2=A2,apin3=A3,apin4=A4,apin5=A5,dpin2=2,dpin13=13,dpin9=9,compin0=6,compin1=7,s10,s11,s12,s13,s14,s15,s16,s17,s18,counter;
-char op,clock_op,op_aux,unit,temp=0;
-float celsius, fahrenheit;
-SoftwareSerial mySerial(10,11,1); // RX, TX
-OneWire  ds(10);
+  Software serial multiple serial test
 
-byte i;
-byte present = 0;
-byte type_s;
-byte data[9];
-byte addr[8];
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
 
-ISR(ANALOG_COMP_vect){
-  cli();
-  digitalWrite(dpin13,LOW);
-  op='a';
-}
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
 
-ISR(TIMER1_COMPA_vect){
-  cli();
-  digitalWrite(dpin2,HIGH);
-  sei();
-}
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo and Micro support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+
+ */
+#include <SoftwareSerial.h>
+#include <stdio.h>
+//#define ID 0x3101
+int s10,s11,s12,s13,s20,s21,s22,s23;
+
+SoftwareSerial mySerial(10, 11,1); // RX, TX
 
 void setup() {
+  // Open serial communications and wait for port to open:  
   Serial.begin(9600);
-  pinMode(dpin13,OUTPUT);
-  pinMode(dpin2,OUTPUT);
-  pinMode(dpin9,OUTPUT);  //0C1A.
-  pinMode(compin0,INPUT);
-  pinMode(compin1,INPUT);
-  //Clock:  
-  TCCR1A=0;
-  TCCR1B=0;
-  OCR1A=15264; //1Hz at 1024 prescaler.
-  TCCR1A|=(1<<COM1A0);
-  TCCR1B|=(1<<WGM12);
-  TCCR1B|=(1<<CS12)|(1<<CS10);
-  TIMSK1 |= (1 << OCIE1A);
-  ACSR=bit(ACIE);
-  mySerial.begin(57600);
-    while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
-    }
-  //sei();
-  // put your setup code here, to run once:
-}// del setup.
+  }  
+   // set the data rate for the SoftwareSerial port  
+  mySerial.begin(9600);  
+}
 
 void loop() {
-  digitalWrite(dpin13,HIGH);
-  if(op=='a'){
-    Serial.print("Pico de voltaje detectado, presione x para reestablecer.");
-    do{
-      op=Serial.read();
-      //sei();
-    }while(op!='x');    
-  }// del if.
-  // put your main code here, to run repeatedly:  
-}// del loop.
-
-void serialEvent(){
-  op=Serial.read();
-  Serial.print("\n\n 0 CAD0.\n 1 CAD1.\n 2 CAD2.\n 3 CAD3.\n 4 CAD4.\n 5 CAD5.\n 6 Clock off.\n 7 Calamp R/D.\n 8 TopFly R/D.\n 9 One Wire. \n\n");
-  
-  switch(op){
-    case '0':
-    do{
-      val=analogRead(apin0);
-      Serial.println(val); 
-      op=Serial.read();  
-      }while(op!='x');// del do.
-    break;
-
-    case '1':
-    do{
-      val=analogRead(apin1);
-      Serial.println(val); 
-      op=Serial.read();  
-      }while(op!='x');// del do.
-    break;
-
-    case '2':
-    do{
-      val=analogRead(apin2);
-      Serial.println(val); 
-      op=Serial.read();  
-      }while(op!='x');// del do.
-    break;
-
-    case '3':
-    do{
-      val=analogRead(apin3);
-      Serial.println(val); 
-      op=Serial.read();  
-      }while(op!='x');// del do.
-    break;
-
-    case '4':
-    do{
-      val=analogRead(apin4);
-      Serial.println(val); 
-      op=Serial.read();  
-      }while(op!='x');// del do.
-    break;
-
-    case '5':
-    do{
-      val=analogRead(apin5);
-      Serial.println(val); 
-      op=Serial.read();  
-      }while(op!='x');// del do.
-    break;
-
-    case '6':
-    TCCR1B=0b00000000;
-    break;
-
-    case '7':
-    Serial.print("Type x to end. You must wait until 4 minutes to end the loop.");
-    counter=4; // calamp 4, TopFly 1.
-
-    do{      
-      //Rising.
-      op_aux=Serial.read();
-      for (int i=0;i<counter;i++){
-        s10=62; //hex=3E  Prefix
-        mySerial.write(s10);
-        delay(10);
-
-        s11=1; //hex=01   Sender network address
-        mySerial.write(s11);
-        delay(10);
-
-        s12=7; //hex=07   Command code
-        mySerial.write(s12);
-        delay(10);
-
-        s13=39; //hex=27    Temperature
-        mySerial.write(s13);
-        delay(10);
-
-        s14=255; //hex=FF    User value 01
-        mySerial.write(s14);
-        delay(10);
-
-        s15=15; //hex=0F   User value 02
-        mySerial.write(s15);
-        delay(10);
-
-        s16=255; //hex=FF  Tech user value 01
-        mySerial.write(s16);
-        delay(10);
-
-        s17=255; //hex=FF    Tech user value 02
-        mySerial.write(s17);
-        delay(10);
-
-        s18=84; //hex=54    CRC
-        mySerial.write(s18);
-        delay(10);
-        }//Rising.
-
-        delay(60000); //1 minute.
-
-        //Dropping.
-
-      for(int i=0;i<counter;i++){
-        s10=62; //hex=3E  Prefix
-        mySerial.write(s10);
-        delay(10);
-
-        s11=1; //hex=01   Sender network address
-        mySerial.write(s11);
-        delay(10);
-
-        s12=7; //hex=07   Command code
-        mySerial.write(s12);
-        delay(10);
-
-        s13=35; //hex=23    Temperature
-        mySerial.write(s13);
-        delay(10);
-
-        s14=220; //hex=DC    User value 01
-        mySerial.write(s14);
-        delay(10);
-
-        s15=2; //hex=02   User value 02
-        mySerial.write(s15);
-        delay(10);
-
-        s16=201; //hex=C9  Tech user value 01
-        mySerial.write(s16);
-        delay(10);
-
-        s17=45; //hex=2D    Tech user value 02
-        mySerial.write(s17);
-        delay(10);
-
-        s18=207; //hex=CF    CRC
-        mySerial.write(s18);
-        delay(10);
-        }//Dropping.
-
-        delay(60000); //1 minute.
-      }while(op_aux!='x');          
-    break;
-    
-    case '8':
-    Serial.print("Type x to end. You must wait until 4 minutes to end the loop.");
-    counter=1; // calamp 4, TopFly 1.
-
-    do{      
-      //Rising.
-      op_aux=Serial.read();
-      for (int i=0;i<counter;i++){
-        s10=62; //hex=3E  Prefix
-        mySerial.write(s10);
-        delay(10);
-
-        s11=1; //hex=01   Sender network address
-        mySerial.write(s11);
-        delay(10);
-
-        s12=7; //hex=07   Command code
-        mySerial.write(s12);
-        delay(10);
-
-        s13=39; //hex=27    Temperature
-        mySerial.write(s13);
-        delay(10);
-
-        s14=255; //hex=FF    User value 01
-        mySerial.write(s14);
-        delay(10);
-
-        s15=15; //hex=0F   User value 02
-        mySerial.write(s15);
-        delay(10);
-
-        s16=255; //hex=FF  Tech user value 01
-        mySerial.write(s16);
-        delay(10);
-
-        s17=255; //hex=FF    Tech user value 02
-        mySerial.write(s17);
-        delay(10);
-
-        s18=84; //hex=54    CRC
-        mySerial.write(s18);
-        delay(10);
-        }//Rising.
-
-        delay(60000); //1 minute.
-
-        //Dropping.
-
-      for(int i=0;i<counter;i++){
-        s10=62; //hex=3E  Prefix
-        mySerial.write(s10);
-        delay(10);
-
-        s11=1; //hex=01   Sender network address
-        mySerial.write(s11);
-        delay(10);
-
-        s12=7; //hex=07   Command code
-        mySerial.write(s12);
-        delay(10);
-
-        s13=35; //hex=23    Temperature
-        mySerial.write(s13);
-        delay(10);
-
-        s14=220; //hex=DC    User value 01
-        mySerial.write(s14);
-        delay(10);
-
-        s15=2; //hex=02   User value 02
-        mySerial.write(s15);
-        delay(10);
-
-        s16=201; //hex=C9  Tech user value 01
-        mySerial.write(s16);
-        delay(10);
-
-        s17=45; //hex=2D    Tech user value 02
-        mySerial.write(s17);
-        delay(10);
-
-        s18=207; //hex=CF    CRC
-        mySerial.write(s18);
-        delay(10);
-        }//Dropping.
-
-        delay(60000); //1 minute.
-      }while(op_aux!='x');          
-    break;  
-
-    case '9':
-      if ( !ds.search(addr)) {
-        Serial.println("No more addresses.");
-        Serial.println();
-        ds.reset_search();
-        delay(250);
-        //return;
-      }
-      Serial.print("ROM =");
-      for( i = 0; i < 8; i++) {
-        Serial.write(' ');
-        Serial.print(addr[i], HEX);
-      }
-
-      if (OneWire::crc8(addr, 7) != addr[7]) {
-        Serial.println("CRC is not valid!");
-        //return;
-      }
-      Serial.println();
-
-        switch (addr[0]) {
-
-          case 0x10:
-          Serial.println("Chip = DS18S20");  // or old DS1820
-          type_s = 1;
-          break;
-
-          case 0x42:
-          Serial.println("TSO Mobile.");
-          type_s = 0;
-          break;
-
-          case 0x22:
-          Serial.println("Chip = DS1822");
-          type_s = 0;
-          break;
-
-          case 0x28:
-          Serial.println("Chip = Temperature Sensor");
-          temp=1;
-          type_s=0;
-          break;
-
-          default:
-          Serial.println("Device is not a DS18x20 family device.");
-          type_s=0;
-          //return;        
-        }
-
-    ds.reset();
-    ds.select(addr);
-    ds.write(0x44, 1);        // start conversion, with parasite power on at the end.
-    
-    delay(1000);     // maybe 750ms is enough, maybe not.
-    // we might do a ds.depower() here, but the reset will take care of it.
-    
-    present = ds.reset();
-    ds.select(addr);    
-    ds.write(0xBE);         // Read Scratchpad.
-
-    Serial.print("Data = ");
-    Serial.print(present, HEX);
-    Serial.print(" ");
-    for ( i = 0; i < 9; i++) {           // we need 9 bytes.
-      data[i] = ds.read();
-      Serial.print(data[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.print(" CRC=");
-    Serial.print(OneWire::crc8(data, 8), HEX);
-    Serial.print("\nPress reset on Arduino + option 9 to read another device.");
-
-    // Convert the data to actual temperature
-    // because the result is a 16 bit signed integer, it should
-    // be stored to an "int16_t" type, which is always 16 bits
-    // even when compiled on a 32 bit processor.
-    int16_t raw = (data[1] << 8) | data[0];
-    if (type_s) {
-      raw = raw << 3; // 9 bit resolution default.
-      if (data[7] == 0x10) {
-        // "count remain" gives full 12 bit resolution.
-        raw = (raw & 0xFFF0) + 12 - data[6];
-      }
-    } else {
-      byte cfg = (data[4] & 0x60);
-      // at lower res, the low bits are undefined, so let's zero them.
-      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms.
-      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms.
-      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms.
-      //// default is 12 bit resolution, 750 ms conversion time.
-    }
-    
-    if (temp==1){
-      celsius = (float)raw / 16.0;
-    fahrenheit = celsius * 1.8 + 32.0;
-    Serial.print("Temperature = ");
-    Serial.print(celsius);
-    Serial.print("Celsius, ");
-    Serial.print(fahrenheit);
-    Serial.println("Fahrenheit");    
-    temp=0;
-    }
-
-    break;    
-
-    default:
-    Serial.print(" Fin");    
-  }// Switch.  
- 
-}// SerialEvent.
+    s10=49; //hex=31
+    mySerial.write(s10);
+    delay(10);
+    s11=1; //hex=01
+    mySerial.write(s11);
+    delay(10);
+    s12=6; //hex=06
+    mySerial.write(s12);
+    delay(10);
+    s13=108; //hex=6C
+    mySerial.write(s13);
+    delay(1000);
+    s20=49; //hex=31
+    mySerial.write(s20);
+    delay(10);
+    s21=2; //hex=01
+    mySerial.write(s21);
+    delay(10);
+    s22=6; //hex=06
+    mySerial.write(s22);
+    delay(10);
+    s23=57; //hex=6C
+    mySerial.write(s23);
+    delay(1000);
+    /*
+    mySerial.print(a);         // Print as an ASCII-encoded decimal
+    mySerial.print("\t");                // Print a tab character
+    mySerial.print(a, DEC);    // Print as an ASCII-encoded decimal
+    mySerial.print("\t");                // Print a tab character
+    mySerial.print(a, HEX);    // Print as an ASCII-encoded hexadecimal
+    mySerial.print("\t");                // Print a tab character
+    mySerial.print(a, OCT);    // Print as an ASCII-encoded octal
+    mySerial.print("\t");                // Print a tab character
+    mySerial.print(a, BIN);    // Print as an ASCII-encoded binary
+    mySerial.print("\t");                // Print a tab character
+    //mySerial.print(a/4, BYTE);
+    */             
+}
---
+++ Arduino/zonar_tool_nano/zonar_tool_nano.ino
@@ -0,0 +1,196 @@
+#include <OneWire.h>
+int sensorPin = A0;   // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+char op;
+int cont=0;
+OneWire ds(10);
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  while (!Serial);
+  Serial.print("1. Analog sensor A0. \n2. Fuel sensor. \n3. OneWire. \nx. Exit!\n");
+}// void setup.
+
+void loop() { 
+
+  op=Serial.read();
+  switch(op){
+    case '1':
+    do{
+    // read the value from the sensor:
+    sensorValue=analogRead(sensorPin);
+    Serial.println(sensorValue);
+    op=Serial.read();
+    }while(op!='x');
+    break;
+
+    case '2':
+    Serial.print("Rising!");
+    Serial1.write(06); //HEX=3E
+    delay(10);
+    //Sender network address.
+    Serial1.write(64); //HEX=01
+    delay(10);
+    //Command code.
+    Serial1.write(136);//HEX=07
+    delay(10);
+    //Temperature.
+    Serial1.write(52); //HEX=27
+    delay(2000);
+    //User value of fuel level 1
+    Serial1.write(58); //HEX=FF
+    delay(10);
+    //User value of fuel level 2
+    Serial1.write(96); //HEX=0F
+    delay(10);
+    //Technological value of fuel level 1
+    Serial1.write(152);//HEX=FF
+    delay(10);
+    //Technological value of fuel level 2
+    Serial1.write(141); //HEX=FF
+    delay(2000);
+    //CRC.
+    Serial1.write(59); //HEX=54
+    delay(2000);
+
+    Serial.print("Droping!");
+    Serial1.write(06); //HEX=3E
+    delay(10);
+    //Sender network address.
+    Serial1.write(64); //HEX=01
+    delay(10);
+    //Command code.
+    Serial1.write(136);//HEX=07
+    delay(10);
+    //Temperature.
+    Serial1.write(52); //HEX=23
+    delay(2000);
+    //User value of fuel level 1
+    Serial1.write(58); //HEX=DC
+    delay(10);
+    //User value of fuel level 2
+    Serial1.write(96); //HEX=02
+    delay(10);
+    //Technological value of fuel level 1
+    Serial1.write(152);//HEX=C9
+    delay(10);
+    //Technological value of fuel level 2
+    Serial1.write(141); //HEX=2D
+    delay(2000);
+    //CRC.
+    Serial1.write(59); //HEX=CF
+    delay(2000);
+
+    do{
+    op=Serial.read();
+    }while(op!='x');
+    break;
+
+    case '3':    
+    byte i;
+    byte present = 0;
+    byte type_s;
+    byte data[9];
+    byte addr[8];
+    float celsius, fahrenheit;
+    
+    if ( !ds.search(addr)) {
+      Serial.println("No more addresses.");
+      Serial.println();
+      ds.reset_search();
+      delay(250);
+      //return;                                                 Commented
+    }// if
+    
+    Serial.print("ROM =");
+    for( i = 0; i < 8; i++) {
+      Serial.write(' ');
+      Serial.print(addr[i], HEX);
+    }// if
+
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        Serial.println("CRC is not valid!");
+        //return;                                               Commented
+    }// if
+    Serial.println();
+  
+    // the first ROM byte indicates which chip
+    switch (addr[0]) {
+      case 0x10:
+        Serial.println("  Chip = DS18S20");  // or old DS1820
+        type_s = 1;
+        break;
+      case 0x42:
+        Serial.println("  Chip = DS18B20");
+        type_s = 0;
+        break;
+      case 0x28:
+        Serial.println("  Chip = DS1822");
+        type_s = 0;
+        break;
+      default:
+        Serial.println("Device is not a DS18x20 family device.");
+        //return;                                               Commented
+    }// switch 
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+    
+    delay(1000);     // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
+    
+    present = ds.reset();
+    ds.select(addr);    
+    ds.write(0xBE);         // Read Scratchpad
+
+    Serial.print("Data = ");
+    Serial.print(present, HEX);
+    Serial.print(" ");
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+      data[i] = ds.read();
+      Serial.print(data[i], HEX);
+      Serial.print(" ");
+    }// for
+    Serial.print(" CRC=");
+    Serial.print(OneWire::crc8(data, 8), HEX);
+    Serial.println();
+
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s) {
+      raw = raw << 3; // 9 bit resolution default
+      if (data[7] == 0x10) {
+        // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
+      }// if
+    } else {
+      byte cfg = (data[4] & 0x60);
+      // at lower res, the low bits are undefined, so let's zero them
+      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+      //// default is 12 bit resolution, 750 ms conversion time
+    }// if - else
+    celsius = (float)raw / 16.0;
+    fahrenheit = celsius * 1.8 + 32.0;
+    Serial.print("Temperature = ");
+    Serial.print(celsius);
+    Serial.print(" Celsius, ");
+    Serial.print(fahrenheit);
+    Serial.println(" Fahrenheit");
+    do{
+    op=Serial.read();
+    }while(op!='x');
+    break;
+    }// switch
+  
+}//void loop
+
