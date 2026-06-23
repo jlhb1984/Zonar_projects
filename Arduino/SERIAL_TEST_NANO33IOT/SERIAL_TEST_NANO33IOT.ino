ae08a52338e60d6c9898593619cfd6336f1e9441
Jose Luis Hurtado Balcazar <jose.hurtado@zonarsystems.com>
2026-06-23 08:55:24 -0500

    Succesful test with CALIBRATION_TOOL. Edition on SERIAL_TEST_NANO33IOT

--- Arduino/SERIAL_TEST_NANO33IOT/SERIAL_TEST_NANO33IOT.ino
+++ Arduino/SERIAL_TEST_NANO33IOT/SERIAL_TEST_NANO33IOT.ino
@@ -6,47 +6,12 @@ void setup() {
 
 void loop() {
  
-  /*
+  
   for (int i=0; i<256; i++){ 
   Serial1.write(i);
-  delay(1000);
-  }
-  */
-
-  //Prefix.
-  Serial1.write(06); //HEX=3E
   delay(10);
-
-  //Sender network address.
-  Serial1.write(64); //HEX=01
-  delay(10);
-
-  //Command code.
-  Serial1.write(136);//HEX=07
-  delay(10);
-
-  //Temperature.
-  Serial1.write(52); //HEX=19
-  delay(2000);
-
-  //User value of fuel level 1
-  Serial1.write(58); //HEX=0C
-  delay(10);
-  //User value of fuel level 2
-  Serial1.write(96); //HEX=04
-  delay(10);
-
-  //Technological value of fuel level 1
-  Serial1.write(152);//HEX=C7
-  delay(10);
-  //Technological value of fuel level 2
-  Serial1.write(141); //HEX=40
-  delay(2000);
-
-  //CRC.
-  Serial1.write(59); //HEX=62
-  delay(2000);
-
+  }  
+  
   /*
   // read from port 1, send to port 0:
   if (Serial1.available()) {
